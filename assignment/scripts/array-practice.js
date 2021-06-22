445a6e27cf3ff10847e773b7cb9ec5d6637235c6
Curtis Kettler <curtiskettler@gmail.com>
2021-06-22 09:52:55 -0500

    completed week 3 assignment 1

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -38,7 +38,8 @@ console.log('Last animal is', animalArray[3]);
 
 // 3.c. (STRETCH) TODO: Log the last animal by using the array length,
 //      instead of the exact index number of the last item
-
+let lastAnimal = animalArray[animalArray.length - 1];
+console.log('The last animal is still', lastAnimal);
 
 // 4. Adding & Removing Array Items
 console.log('--- 4. Adding and removing array items ---');
